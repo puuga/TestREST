34d199110439270dff781ae37a89eba3b6322b83
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-05-04 16:05:13 +0700

    Initial Commit

---
+++ TestREST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestREST
+//
+//  Created by Siwawes Wongcharoen on 5/4/2559 BE.
+//  Copyright © 2559 puuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
